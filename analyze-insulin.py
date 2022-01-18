fc786f71ac75f94350b2ec0969d170403507a1db
dev-jamesbrown <97547175+dev-jamesbrown@users.noreply.github.com>
2022-01-18 13:59:59 -0500

    Create analyze-insulin.py

---
+++ analyze-insulin.py
@@ -0,0 +1,23 @@
+preproinsulin = "malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktrreaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"
+print("preproinsulin is")
+print(preproinsulin)
+Isinsulin = ""
+for char in preproinsulin [0 : 24]:
+    Isinsulin = Isinsulin+char
+print("Isinsulin is")
+print(Isinsulin)
+Binsulin = ""
+for char in preproinsulin [24 : 54]:
+    Binsulin = Binsulin+char
+print("Binsulin is")
+print(Binsulin)
+Cinsulin = ""
+for char in preproinsulin [54 : 89]:
+    Cinsulin = Cinsulin+char
+print("Cinsulin is")
+print(Cinsulin)
+Ainsulin = ""
+for char in preproinsulin [89 : 110]:
+    Ainsulin = Ainsulin+char
+print("Ainsulin is")
+print(Ainsulin)
